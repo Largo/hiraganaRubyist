05a1271c6a9f5bb764897c354210df851c1afcf6
Largo <largoko@gmail.com>
2018-05-30 17:04:58 +0900

    add all the infos

--- checkme.rb
+++ checkme.rb
@@ -1,15 +1,18 @@
-require 'mojinizer'
+#require 'mojinizer'
 require_relative 'kana.rb'
 
-$monographs.flatten(1).shuffle.each do |a|
-    romaji = a[0]
-    hiragana = a[1]
-    katakana = a[2]
+dataset = $monographs.concat($digraphs).concat($monographs_with_diacritics).concat($digraphs_with_diacritics)
 
-    puts katakana
-    sleep 3
-    puts romaji
-    sleep 0.5
-    puts "\n"
+dataset.flatten(1).shuffle.each do |a|
+  if not a[0].empty?
+      romaji = a[0]
+      hiragana = a[1]
+      katakana = a[2]
 
+      puts katakana
+      sleep 3
+      puts romaji
+      sleep 0.5
+      puts "\n"
+  end
 end
